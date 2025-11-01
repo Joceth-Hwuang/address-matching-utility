21ba04dac48c7513cf9f255d1e3a9b58e4b66bc2
Joe Wong <lance.joe@outlook.com>
2025-11-01 13:43:39 +0800

    oop

--- process.py
+++ process.py
@@ -13,40 +13,41 @@ import pandas as pd
 # In[ ]:
 
 
-# streamlit
+# streamlit UI display
 
-# header
-st.title("Address Matching with SBERT-LaBSE")
+class AppUserInterface:
+    
+    uploaded_file = []
+    
+    @classmethod
+    def execute(cls):
+        """
+        main method
+        """
+        cls.headings()
+        cls.upload_widget()
+        while not uploaded_file:
+            cls.upload_widget()
+    
+    @classmethod    
+    def headings(cls):    
+        st.title("Address Matching with SBERT-LaBSE")
+        st.write("Murdoch MSc. IT TSA2025 ICT619 Project Assignment")
+        st.write("Group1 Student IDs: 35370919, 35427755, 35059342")
+        st.divider()
+        st.write("Please upload your excel workbook in this format")
+        st.write("Column A = Address Unique ID. This helps you identify the address")
+        st.write("Column B = Source Address")
+        st.write("Column C = Target Address")
+        st.divider()
 
-# header-description
-st.write("Murdoch MSc. IT TSA2025 ICT619 Project Assignment")
-st.write("Group1 Student IDs: 35370919, 35427755, 35059342")
-st.divider()
-
-# file upload
-st.write("Please upload your excel workbook in this format")
-st.write("Column A = Address Unique ID. This helps you identify the address")
-st.write("Column B = Source Address")
-st.write("Column C = Target Address")
-st.divider()
-
-# file upload widget
-file_uploaded = st.file_uploader("Upload an Excel Workbook", type=["xlsx", "xls"])
-
-
-# In[ ]:
-
-
-# extract
-if file_uploaded:
-    read_df = pd.read_excel(file_uploaded)
-    st.write("Preview of uploaded data")
-    st.dataframe(df.head(5))
-else: 
-    st.write("File not uploaded")
-
-
-# open dataframe
+    @classmethod
+    def upload_widget(cls):
+        cls.uploaded_file.append(
+        st.file_uploader("Upload an Excel Workbook", type=["xlsx", "xls"])
+        )
+    
+    
 
 
 # In[ ]:
